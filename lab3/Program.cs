3174fd06edfa013e61b38d0f077611f5afcab865
Александр Немеров <nemerov_a@mail.ru>
2022-10-02 20:19:01 +0800

    7898754645

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -17,55 +17,109 @@ namespace lab3
              Вывод на дисплей информации о людях, чьи дни рождения приходятся раньше даты,
             введенной с клавиатуры. Если таких нет, вывести сообщение об этом.
             Напишите соответствующие методы для этого.*/
+            List<Note> list = new List<Note>();
+            while (true)
+            {
+                Console.WriteLine("1 - Добавить\n2 - Выести всех\n3 - Вывести по дате рождения");
+                int switcher = Convert.ToInt32(Console.ReadLine());
+                switch (switcher)
+                {
+                    case 0:
+                        {
+                            return;
+                        }
+                    case 1:
+                        {
+                            try
+                            {
+                                Console.Write("Фамилия - ");
+                                string? Surname = Console.ReadLine();
+                                Console.Write("Имя - ");
+                                string? Name = Console.ReadLine();
+                                Console.Write("День рождения - ");
+                                int Day = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("Месяц рождения - ");
+                                int Month = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("Год рождения - ");
+                                int Year = Convert.ToInt32(Console.ReadLine());
+                                list.Add(new Note(Surname, Name, Day, Month, Year));
+                                break;
+                            } catch(Exception e) { Console.WriteLine(e.ToString()); break; }
+                        }
+                    case 2:
+                        {
+                            for(int i = 0; i < list.Count; i++)
+                            {
+                                Console.WriteLine(list[i].ToString());
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.Write("День рождения - ");
+                            int Day = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Месяц рождения - ");
+                            int Month = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Год рождения - ");
+                            int Year = Convert.ToInt32(Console.ReadLine());
+                            list[0].printBeforeBorn(list, Day, Month, Year);
+                            break;
+                        }
+                    default: break;
+                }
+            }
         }
         struct Note
         {
-            string Surname;
-            string Name;
-            string Born;
+            string? Surname;
+            string? Name;
+            string? Born;
             private int Day;
             private int Month;
             private int Year;
-            public Note(string Surname, string Name, int Day, int Month, int Year)
+            public Note(string? Surname, string? Name, int Day, int Month, int Year)
             {
                 this.Surname = Surname;
                 this.Name = Name;
-                this.Day = (Day >= 30 && Day <= 1) ? Day : 1;
-                this.Month = (Month >= 12 && Month <= 1) ?  Month : 1;
-                this.Year = (Year >= 2023 && Year <= 1900) ? Year : 2023;
+                this.Day = (Day >= 30 && Day <= 1) ? 1 : Day;
+                this.Month = (Month >= 12 && Month <= 1) ?  1 : Month;
+                this.Year = (Year >= 2023 && Year <= 1900) ? 2023 : Year;
                 this.Born = Day + "." + Month + "." + Year;
             }
-            public void printBeforeBorn(Note[] notes, int Day, int Month, int Year)
+            public void printBeforeBorn(List<Note> list, int Day, int Month, int Year)
             {
                 bool searched = false;
-                for (int i = 0; i < notes.Length; i++)
+                for (int i = 0; i < list.Count; i++)
                 {
-                    if (notes[i].Year >= Year)
+                    
+                    if (list[i].Year > Year)
                     {
-                        Console.WriteLine("Фамилия " + notes[i].Surname + " | Имя "
-                            + notes[i].Name + " | Родился(лась) " + notes[i].Born);
+                        Console.WriteLine(list[i].ToString());
                         searched = true;
                         continue;
                     } 
-                    else if (notes[i].Month >= Month)
+                    else if (list[i].Month > Month)
                     {
-                        Console.WriteLine("Фамилия " + notes[i].Surname + " | Имя "
-                            + notes[i].Name + " | Родился(лась) " + notes[i].Born);
+                        Console.WriteLine(list[i].ToString());
                         searched = true;
                         continue;
                     } 
-                    else if (notes[i].Day >= Day)
+                    else if (list[i].Day > Day)
                     {
-                        Console.WriteLine("Фамилия " + notes[i].Surname + " | Имя "
-                            + notes[i].Name + " | Родился(лась) " + notes[i].Born);
+                        Console.WriteLine(list[i].ToString());
                         searched = true;
                         continue;
                     }
                 }
                 if (!searched)
                 {
-
+                    Console.WriteLine("Ошибка не найдено");
                 }
+            }
+            public override string? ToString()
+            {
+                return "Фамилия " + this.Surname + " | Имя "
+                            + this.Name + " | Родился(лась) " + this.Born;
             }
         }
         static void Task2()
@@ -79,6 +133,20 @@ namespace lab3
             ширину и высоту. То есть получить ширину и высоту из объекта должно быть можно только через
             декомпозицию.
             Продемонстрируйте работу класса.*/
+
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine("Периметр: " + rectangle.Perimeter() + " Периметр: " + rectangle.Square());
+            int width, height;
+            rectangle.Deconstruct(out width, out height);
+            Console.WriteLine("Высота: " + height + " Длина: " + width);
+
+
+
+            rectangle = new Rectangle(657, 983);
+            Console.WriteLine("Периметр: " + rectangle.Perimeter() + " Периметр: " + rectangle.Square());
+            rectangle.Deconstruct(out width, out height);
+            Console.WriteLine("Высота: " + height + " Длина: " + width);
+            Console.ReadKey();
         }
     }
 }
---
+++ lab3/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Rectangle
+    {
+        private int width, height;
+        public Rectangle()
+        {
+            Random random = new Random();
+            this.width = random.Next(0,100);
+            this.height = random.Next(0,100);
+        }
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public void Deconstruct(out int width, out int height)
+        {
+            width = this.width;
+            height = this.height;
+        }
+        public int Square()
+        {
+            return width * height;
+        }
+        public int Perimeter()
+        {
+            return (width + height) * 2;
+        }
+    }
+}
